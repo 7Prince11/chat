b5b714bb84425e5071d454fd4418b61fbd7fdafb
Oleksandr <fazzytron@gmail.com>
2022-10-19 17:16:31 +0200

    Chat

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const process = require('process');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const io = require('socket.io')(port, {
 
 
